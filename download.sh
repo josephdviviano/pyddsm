b538a6ea8a7c6d4fc3a1ba768a578823b47ed45b
Joseph Viviano <vivianoj@leto38.iro.umontreal.ca>
2018-02-03 11:33:44 -0500

    added cleanup command to download

--- download.sh
+++ download.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+
+echo "all data will be downloaded to: ${PWD}"
+
 # downloading achieved via anon ftp (ncftpget), links tested Jan 2018
 while read line; do
     echo "accessing: ${line} @ $(date)"
     ncftpget -R ${line}
 done < ftp_list.txt
 
-
+# clean up
+find . -name *.gz -exec gunzip {} \;
